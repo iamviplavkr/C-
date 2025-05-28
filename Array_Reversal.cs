b8bb841d4637157df6f69086f103843e1b457a20
ViPlav Kumar <155620870+iamviplavkr@users.noreply.github.com>
2025-05-28 15:06:26 +0530

    Create Array_Reversal.cs

---
+++ Array_Reversal.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                4,5,33,4,55,6,1,7,77
+            };
+
+            Array.Reverse(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+                
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
